fedf97843e913720cf1b37b90f2b6d905629b810
xingchuan <xingchuan@pinduoduo.com>
2020-07-15 17:23:30 +0800

    update

--- src/main/java/DfsBfs127_ladderLength.java
+++ src/main/java/DfsBfs127_ladderLength.java
@@ -4,9 +4,9 @@ public class DfsBfs127_ladderLength {
     Map<String, List<String>> pattern2strs = new HashMap<>();
     String endWord;
     HashSet<String> visited = new HashSet<>();
+    int res = Integer.MAX_VALUE;
 
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        if (!wordList.contains(endWord)) return 0;
         this.endWord = endWord;
         for (String s : wordList) {
             for (String pattern : getPatterns(s)) {
@@ -18,35 +18,32 @@ public class DfsBfs127_ladderLength {
                 list.add(s);
             }
         }
-        return dfs(beginWord);
+        dfs(beginWord, 1);
+        return res == Integer.MAX_VALUE ? 0 : res;
     }
 
-    private int dfs(String curr) {
+    private void dfs(String curr, int count) {
         if (curr.equals(endWord)) {
-            return 1;
-        }
-        int min = Integer.MAX_VALUE;
-        List<String> nextWords = new ArrayList<>();
-        for (String pattern : getPatterns(curr)) {
-            List<String> list = pattern2strs.get(pattern);
-            if (list != null) {
-                for (String s : list) {
-                    if (!s.equals(curr)) {
-                        nextWords.add(s);
+            if (count < res) res = count;
+        } else {
+            List<String> nextWords = new ArrayList<>();
+            for (String pattern : getPatterns(curr)) {
+                List<String> list = pattern2strs.get(pattern);
+                if (list != null) {
+                    for (String s : list) {
+                        if (!s.equals(curr)) {
+                            nextWords.add(s);
+                        }
                     }
                 }
             }
+            for (String nextWord : nextWords) {
+                if (visited.contains(nextWord)) continue;
+                visited.add(nextWord);
+                dfs(nextWord, count + 1);
+                visited.remove(nextWord);
+            }
         }
-        boolean hasNext = false;
-        for (String nextWord : nextWords) {
-            if (visited.contains(nextWord)) continue;
-            hasNext = true;
-            visited.add(nextWord);
-            int dfsVal = dfs(nextWord);
-            visited.remove(nextWord);
-            if (dfsVal < min) min = dfsVal;
-        }
-        return hasNext ? min + 1 : 0;
     }
 
     private List<String> getPatterns(String s) {
@@ -62,7 +59,8 @@ public class DfsBfs127_ladderLength {
     }
 
     public static void main(String[] args) {
-        int i = new DfsBfs127_ladderLength().ladderLength("hit", "cog", Arrays.asList("hot", "dot", "dog", "lot", "log", "cog"));
+//        int i = new DfsBfs127_ladderLength().ladderLength("hit", "cog", Arrays.asList("hot", "dot", "dog", "lot", "log", "cog"));
+        int i = new DfsBfs127_ladderLength().ladderLength("hot", "dog", Arrays.asList("hot", "dog", "dot"));
         System.out.println(i);
     }
 }
