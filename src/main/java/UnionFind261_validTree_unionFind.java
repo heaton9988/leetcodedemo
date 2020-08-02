415c4b84d2892d2b75a4a4a36dc46b8d60301ac6
xingchuan <xingchuan@pinduoduo.com>
2020-08-02 14:30:10 +0800

    update

---
+++ src/main/java/UnionFind261_validTree_unionFind.java
@@ -0,0 +1,66 @@
+import java.util.Arrays;
+
+public class UnionFind261_validTree_unionFind {
+    public boolean validTree(int n, int[][] edges) {
+        DisjointSet disjointSet = new DisjointSet(n);
+        for (int[] edge : edges) {
+            if (!disjointSet.union(edge[0], edge[1]))
+                return false;
+        }
+        return disjointSet.getCount() == 1;
+    }
+
+    //并查集
+    class DisjointSet {
+        int n;
+        int[] parent;
+        int[] rank;
+        int count; //连同分量
+
+        DisjointSet(int n) {
+            this.n = n;
+            this.parent = new int[n];
+            Arrays.fill(parent, -1);
+            this.rank = new int[n];
+            this.count = n;
+        }
+
+        //寻找父节点
+        private int findRoot(int x) {
+            int root = x;
+            while (parent[root] != -1) {
+                root = parent[root];
+            }
+            return root;
+        }
+
+        public boolean union(int x, int y) {
+            int xRoot = findRoot(x);
+            int yRoot = findRoot(y);
+            //如果两个在同一颗树上，直接返回false
+            if (xRoot == yRoot) return false;
+            //压缩路径
+            if (rank[xRoot] > rank[yRoot]) {
+                parent[yRoot] = xRoot;
+            } else if (rank[xRoot] < rank[yRoot]) {
+                parent[xRoot] = yRoot;
+            } else if (rank[xRoot] == rank[yRoot]) {
+                parent[xRoot] = yRoot;
+                rank[yRoot]++;
+            }
+            count--;
+            return true;
+        }
+
+        public int getCount() {
+            return this.count;
+        }
+    }
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        UnionFind261_validTree_unionFind obj = new UnionFind261_validTree_unionFind();
+        System.out.println(obj.validTree(5, new int[][]{{0, 1}, {0, 2}, {0, 3}, {1, 4}}));
+        System.out.println(System.currentTimeMillis() - start + " ms");
+    }
+}
